b53ef0ff9bf20c08576c0915c505837486a99473
jorisgengler <jorisgengler@hotmail.com>
2017-12-01 12:02:03 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,438 @@
+<!DOCTYPE html>
+<!created 6-11-2017>
+<!lekker anders2>
+
+
+<html lang=nl>
+
+<head>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Lekker Anderz</title>
+	<meta charset="utf-8">
+
+
+
+<!-- javascript-->
+
+	<script src="https://use.fontawesome.com/b46fce0e6e.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+	<script type="text/javascript" src="rotateAnimation.js"></script>
+	<script type="text/javascript" src="first page.js"></script>
+
+
+<!-- CSS -->
+	<link rel="STYLESHEET" href="main.css" type="text/css">
+	<link rel="STYLESHEET" href="carousel.css" type="text/css">
+
+</head>
+
+<body>
+  <div id="social">
+         <button onClick="window.open('http://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.developphp.com%2Fvideo%2FFlash%2FWebsite-Guestbook-Tutorial','sharer','toolbar=0,status=0,width=580,height=400');" title="Share on Facebook"></button><button onClick="window.open('http://plus.google.com/share?url=https%3A%2F%2Fwww.developphp.com%2Fvideo%2FFlash%2FWebsite-Guestbook-Tutorial','share','toolbar=0,status=0,width=550,height=450');" title="Share on Google Plus"></button><button onClick="window.open('http://www.twitter.com/intent/tweet?url=https%3A%2F%2Fwww.developphp.com%2Fvideo%2FFlash%2FWebsite-Guestbook-Tutorial&amp;text=Flash - Website Guestbook Tutorial','tweety','toolbar=0,status=0,width=550,height=450');" title="Tweet this"></button><button onClick="window.location='mailto:?subject=Flash - Website Guestbook Tutorial&body=Hi, I thought you might like to see this: %0D%0Ahttps://www.developphp.com/video/Flash/Website-Guestbook-Tutorial';" title="Send this to an email contact"></button>
+
+       </div>
+       <nav>
+     		<ul>
+     		  <li><a href="#home" title="Home" id="default" class="highlite" onclick="highlite(this);"> <i home  class="navbarlogo fa fa-home" aria-hidden="true">  </i> </a></li>
+     			<li><a href="#barista" title="Barista" onclick="highlite(this);"> <i class="navbarlogo fa fa-coffee" aria-hidden="true"></i> </a></li>
+     			<li><a href="#koks" title="Koks" onclick="highlite(this);"> <i class="navbarlogo fa fa-user" aria-hidden="true"></i> </a></li>
+     			<li><a href="#onsaanbod" title="onsaanbod" onclick="highlite(this);"> <i class="navbarlogo fa fa-cutlery" aria-hidden="true"></i> </a></li>
+     			<li><a href="#overons" title="Over ons" onclick="highlite(this);"> <i class="navbarlogo fa fa-users" aria-hidden="true"></i> </a></li>
+     			<li><a href="#contact" title="Contact" onclick="highlite(this);">  <i class="navbarlogo fa fa-address-book" aria-hidden="true"></i>  </a></li>
+     		</ul>
+     	</nav>
+
+
+	<div class="grid">
+	<!-------------- first row -------------->
+	<div id="barista" class="barista">
+<!------------------------------------Section1------------------------------------->
+<div id="section1b" class="section1b">
+</div>
+<!------------------------------------Section2------------------------------------->
+<div id="section2b" class="section2b">
+<img src="images/logo.png" alt="Logo" style="width: 30%; height: auto; ">
+</div>
+<!------------------------------------Section3------------------------------------->
+<div id="section3b" class="section3b">
+</div>
+<!------------------------------------Section4------------------------------------->
+<div id="section4b" class="section4b">
+</div>
+<!------------------------------------Section5------------------------------------>
+<div id="section5b" class="section5b">
+  <img src= "images/Barista1.png"style="width: 90%; height auto; float: center; ">
+	<h3> Onze Barista’s & ons High Tea arrangement</h3>
+
+<p>Bij ons kunt u terecht voor een goede bak koffie en een vers kopje thee.<br>
+
+<p>Omdat koffie zo' n lekker product is hebben we een breed aanbod.<br>
+Bij Lekker en Anders  staan ervaren Baristas achter de toog.<br>
+Zij draaien hun hand niet om voor uw Espresso, Capuccino, Latte of Mocha.</p>
+<p>Ook voor uw thee genoegens hebben we een fantastisch aanbod.<br>
+Kom langs om een aantal varianten te proeven.</p>
+<p>U kunt bij ons ook een reservering maken om te genieten van een Old Fashioned High Tea arrangement. </p>
+
+
+	<div class="randjeb">
+
+
+</div>
+</div>
+
+<!------------------------------------section6------------------------------------>
+<div id="section6b" class="section6b">
+
+  <img src= "images/Barista.jpg"style="width: 80%; height auto; float: center; ">
+   </br>
+ <img src= "images/Barista2.jpg"style="width: 80%; height auto; float: center; ">
+   </br>
+ <img src= "images/high thea.jpg"style="width: 80%; height auto; float: center; ">
+</div>
+</div>
+
+
+
+	<div id="pagina2" class="pagina2">pagina2</div>
+
+	<div id="koks" class="koks">
+		<div class="kokspaginagrid">
+      <div id="section1koks" class="section1koks">
+        <h1a>Bjorn</h1a>
+          <center>
+<!-- <img src="https://res.cloudinary.com/dvswwojqq/image/upload/c_scale,h_300,w_300/v1511353535/Bjorn-Borg_tqq2ca.jpg"></img> -->
+
+            <video width="420" height="340" autoplay loop>
+             <source src="WhatsApp.mp4" type="video/mp4">
+             <source src="WhatsApp" type="video/ogg">
+           Your browser does not support the video tag.
+           </video>
+
+
+        </center>
+          <p>Bjorn is onze barbecue ster en hij mag graag van alles op het gloeiendhete rooster gooien. Daarvoor zoekt hij naar recepten van over de hele wereld. Vaak is hij op reis om op de meest afgelegen locaties recepten te proeven en om inspiratie op te doen.
+          Bezoek de website van Bjorn om te weten op welke evenementen hij te vinden is.<br><br>
+          <a href="https://www.BBQ-Bjorn.nl" target="blank"><font color="#ffffff">BBQ-Bjorn</font color></a><br><br>
+        Bjorn start zijn grill op voor ons op donderdag 1 februari, vrijdag 2 februari, zaterdag 3 februari en zondag 4 februari 2018. Bespreek uw plaats op tijd!</p><br>
+      </div>
+
+			<div id="section2koks" class="section2koks">
+
+          <h1a>Lena</h1a>
+          <center><img src="https://res.cloudinary.com/dvswwojqq/image/upload/c_scale,h_300,q_100,w_300/v1510128561/20245467_845620175589864_473780876644479737_n_a6ol8d.jpg"></img></center>
+          <p>Lena heeft een uitmuntend gevoel voor balans. Ze maakt de meest prachtige vegetarische gerechten. Smaakvolle creaties zijn haar specialiteit.
+            Lena is een graag geziene gastoptreder bij Lekker en Anders, het verdient dan ook aanbeveling dat u tijdig reserveert wanneer u van haar kookunsten wilt genieten.
+            U kunt haar creaties nader bestuderen en haar eventueel inschakelen om voor u te koken via haar Facebook-pagina.
+            <br><br>
+            <a href="https://www.facebook.com/levenlifestyle/" target="blank"><font color="#ffffff">LeVen@levenlifestyle</font color></a><br><br>
+            Lena gaat weer voor ons koken op donderdag 25 januari, vrijdag 26 januari, zaterdag 27 januari en zondag 28 januari 2018. Wees tijdig met reserveren.</p><br>
+          </div>
+
+        <div id="section3koks" class="section3koks">
+          <h1a>Thomas</h1a>
+            <center><img src="https://res.cloudinary.com/dvswwojqq/image/upload/c_scale,h_300,w_300/v1511353536/nick--thomas-verslaat-keizer--munnik_oip9wi.jpg"></img></center>
+          <p>Thomas kookt met vis. Er is niets dat hij niet wil proberen. Het is dus koken op hoog niveau. Soms betreft het een recept uit Noorwegen, soms een recept uit Japan, soms wordt de vis gekookt, soms gebakken, soms gerookt. Elke keer wanneer Thomas kookt waant u zich in een andere wereld, een wilde wereld.<br><br>
+            <a href="https://www.thomasdewildevisboer.nl" target="blank"><font color="#ffffff">Thomas de Wilde Visboer</font color></a><br><br>
+            Thomas gaat voor u op de chinese toer. Wilt u zijn creaties proeven kom dan langs op donderdag 15 februari, vrijdag 16 februari, zaterdag 17 februari of zondag 18 februari 2018. Reserveren kan via deze website op de reserveringspagina.</p><br></div>
+
+            <div id="section4koks" class="section4koks">
+
+              <h1a>Willemijn</h1a>
+                <center><img src="https://res.cloudinary.com/dvswwojqq/image/upload/c_scale,w_300/v1511353536/e06f0ab4c8c08e6510ad6c5df776c85e_360x360_center_biqicm.png"></img></center>
+              <p>Willemijn wil graag vergeten groenten en recepten weer onder de aandacht brengen. Zij is zeer geinteresseerd in de traditionele Hollandse keuken. Zij kookt dan ook graag stevige kost. Denk daarbij aan een goede andijviestamppot naar Fries recept of heerlijke snert (erwtensoep) zoals Willemijn's oma uit Zeeland die altijd maakte. Willemijn staat garant voor verrassende, en kwalitatief hoogstaande maaltijden.
+                Op de website van Willemijn vindt u haar recepten en ook allerlei tips uit Grootmoeders tijd.<br><br>
+                <a href="https://www.Willemijnkooktengeeftraad.nl" target="blank"><font color="#ffffff">Willemijn Koster recepten van door de eeuwen heen</font color></a><br><br>
+              Willemijn stampt weer een heerlijk potje voor u op donderdag 8 februari, vrijdag 9 februari, zaterdag 10 februari en zondag 11 februari 2018. <br>Het zit vast en zeker weer stampvol, wees tijdig met reserveren!</p><br>
+            </div>
+
+			</div>
+
+    </div>
+
+
+	<!-------------- second row -------------->
+	<div id="pagina4" class="pagina4">pagina4</div>
+	<div id="home" class="home">
+
+	<div class="grid-home">
+<!------------------------------------Section 1------------------------------------>
+		<div id="section1" class="section1"></div>
+<!------------------------------------Section 2------------------------------------>
+		<div id="section2" class="section2">
+			<img src="images/logo.png" alt="Logo" style="width: 250px; height: auto; max-width: 70%; float: center;"> <br>
+		<!--	<img id="logopijl" src="images/logopijl.png" alt="Logo pijl" style="width: 80px; height: auto; max-width: 25%; float: none; margin-left: 60%;
+						margin-top: -8%;">
+			<script>rotateAnimation("logopijl",30);</script> -->
+		</div>
+
+<!------------------------------------Section 3------------------------------------>
+		<div id="section3" class="section3"></div>
+
+<!------------------------------------Section 4------------------------------------>
+		<div id="section4" class="section4">
+			<div id="carouselachtergrond" class="carouselachtergrond">
+				<div id="carousel">
+					<a href="#contact"> <img src="images-carousel/carousel1.jpg" id="carousel1"  /> </a>
+					<a href="#contact"> <img src="images-carousel/carousel2.jpg" id="carousel2"  /> </a>
+					<a href="#contact"> <img src="images-carousel/carousel3.jpg" id="carousel3"  /> </a>
+					<a href="#contact"> <img src="images-carousel/carousel4.jpg" id="carousel4"  /> </a>
+					<a href="#contact"> <img src="images-carousel/carousel5.jpg" id="carousel5"  /> </a>
+					<a href="#contact"> <img src="images-carousel/carousel6.jpg" id="carousel6"  /> </a>
+					<a href="#contact"> <img src="images-carousel/carousel7.jpg" id="carousel7"  /> </a>
+					<a href="#contact"> <img src="images-carousel/carousel8.jpg" id="carousel8"  /> </a>
+				</div>
+			</div>
+		</div>
+<!------------------------------------Section 5------------------------------------>
+		<div id="section5" class="section5"></div>
+<!------------------------------------Section 6------------------------------------>
+		<div id="section6" class="section6"></div>
+<!------------------------------------Section 7------------------------------------>
+		<div id="section7" class="section7"></div>
+
+	</div>
+
+</div>
+	<div id="pagina6" class="pagina6">pagina6</div>
+	<!-------------- third row -------------->
+	<div id="onsaanbod" class="onsaanbod">
+		<div class="onsaanbodgrid">
+			<div class="section1onsaanbod">
+
+			</div>
+			<div class="section2onsaanbod">
+				<img src="images/logo.png" alt="Logo" style="width: 30%; height: auto; ">
+			</div>
+			<div class="section3onsaanbod">
+
+			</div>
+			<div class="section4onsaanbod">
+
+			</div>
+			<div class="section5onsaanbod">
+<h3>Ontbijt</h3>
+				<br>
+				<p>Kom bij Lekker en Anders ontbijten of neem uw ontbijt mee voor onderweg.<br>
+				Wij verzorgen voor u een compleet ontbijt.</p>
+<br>
+<h3>Lunch</h3> <br>
+			<p>
+			Lunch betekent bij ons dat u kunt kiezen uit een hoeveelheid aan gerechten.<br>
+			Heerlijk belegde broodjes, mooie salades, soep of een broodje uitsmijter.<br>
+			De keuze is aan u.<br>
+			Daarnaast bieden we elke dag van de week iets speciaals aan.<br>
+			</p>
+<br>
+			<h3>Onze lunch specials:</h3> <br>
+<p>
+			Maandag: Mediterrane lunchgerechten.<br>
+			Dinsdag: Er staan een paar heerlijke speciaal-soepen op het menu.<br>
+			Woensdag: Pannenkoeken! Van over de hele wereld.<br>
+			Donderdag: Tosti Thursday – gevarieerde keuze uit diverse tosti’s.<br>
+			Vrijdag: Vegetarische lunchgerechten.<br>
+			Zaterdag: Scandinavische lunchgerechten.<br>
+			Zondag: Saladebar en visschotels, broodjes en een fijn glaasje wijn.
+			</p>
+<br>
+			<h3>Het aanschuifrestaurant</h3><br>
+<p>Elke donderdag-, vrijdag-, zaterdag-, of zondagavond is er een andere kok voor u aan het werk.<br>
+	  De ene keer is dat een gerenommeerde koks of cateraar, de andere keer een getalenteerde beginner.</p>
+<br>
+
+<p>U kunt op deze dagen aanschuiven van 18:00 uur tot 23:00 uur.</p>
+<br>
+
+<p>Wij willen u dan ook graag informeren over deze mensen, die met passie voor u koken. <br>
+	Zie voor onze planning van aankomende avonden en andere bijzonderheden <br>
+	onze webpagina met informatie over de gastoptredens door koks en cateraars.</p>
+
+
+			</div>
+			<div class="section6onsaanbod">
+				<img src= "images-lunch/etagere.jpg"style="width: 50%; height auto; float: center; ">
+				</br>
+				<img src= "images-lunch/Cooking.png"style="width: 50%; height auto; float: center; ">
+				</br>
+				<img src= "images-lunch/tavel.jpg"style="width: 50%; height auto; float: center; ">
+			</div>
+			<div class="section7onsaanbod">
+
+			</div>
+		</div>
+
+	</div>
+
+
+
+	<div id="pagina8" class="pagina8">pagina8</div>
+	<div id="overons" class="overons">
+		<div class="overonsgrid">
+			<div class="section1overons">
+			overons
+			</div>
+			<div class="section2overons">
+
+			</div>
+			<div class="section3overons">
+
+			</div>
+			<div class="section4overons">
+
+			</div>
+			<div class="section5overons">
+				<h3>Lekker en Anders – het idee </h3><br>
+
+<p> Lekker Anders is ontstaan uit het idee dat er op elk moment van de dag, <br>
+	op ontspannen wijze mag worden genoten van een hapje of een drankje. </p>
+<p>Daarnaast is voor ons belangrijk dat Lekker en Anders ook een plek voor ontmoeting en kennismaking is.<br>
+	 Een ontmoeting met uw disgenoten, een kennismaking met nieuwe smaken.</p>
+<br>
+<h3>Lekker en Anders is een combinatie van allerlei soorten eetgelegenheden.</h3><br>
+
+<p>Naast een leuke stek voor uw ontbijt en lunch zijn we ook Coffee Bar,  een Take Away Counter en een Aanschuif- en experimenteerrestaurant.
+Dat betekent dat u van nogal verschillende kaarten kunt kiezen.</p>
+<br>
+<p>Bij Lekker en Anders willen we ook graag aandacht besteden aan de ontwikkelingen binnen de kookwereld.<br>
+	 We hebben dan ook veel interesse in de vegetarische keuken of het koken met seizoensgebonden producten uit de regio.
+				<br>
+			</div>
+			<div class="section6overons">
+
+			</div>
+			<div class="section7overons">
+
+			</div>
+		</div>
+	</div>
+
+	<!-------------- fourth row -------------->
+	<div id="pagina10" class="pagina10">pagina10</div>
+
+<!------------------------------------reservering------------------------------------>
+	<div id="contact" class="contact">
+		<div class="grid-contact">
+			<div class="sectioncontact1">
+				<div class="container">
+					<h1>Reservering</h1>
+						<div class="randje">
+							<div class="contactveld">
+
+								<form  action="action_page.php" method="post">
+<br>
+									<label for="firstname"> </label>
+									<input type="text" id="firstname" name="firstname" placeholder="Uw naam">
+
+									<label for="email">  </label>
+									<input type="text" id="email" name="email" placeholder="Uw E-mail">
+									Aantal personen
+<br>
+									<select name="maand"></option>
+										<option value="aantal personen">aantal personen</option>
+										<option value="1 personen">1 persoon</option>
+										<option value="2 personen">2 personen</option>
+										<option value="3 personen">3 personen</option>
+										<option value="4 personen">4 personen</option>
+										<option value="5 personen">5 personen</option>
+										<option value="6 personen">6 personen</option>
+										<option value="7 personen">7 personen</option>
+										<option value="8 personen">8 personen</option>
+										<option value="9 personen">9 personen</option>
+										<option value="10 personen">10 personen</option>
+										<option value="11 personen">11 personen</option>
+										<option value="12 personen">12 personen</option>
+									</select>
+<br>
+									Kies uw tijd
+<br>
+									<select name="tijd">
+										<option value="Selecteer tijd">Selecteer tijd</option>
+										<option value="15:00-16:00">15:00-16:00</option>
+										<option value="16:00-17:00">16:00-17:00</option>
+										<option value="17:00-18:00">17:00-18:00</option>
+										<option value="18:00-19:00">18:00-19:00</option>
+										<option value="20:00-21:00">20:00-21:00</option>
+										<option value="22:00-23:00">22:00-23:00</option>
+									</select>
+<br>
+									Datum
+<br>
+									<input id="maand" name="maand" type="date" placeholder="maand">
+<br>
+									<label for="subject"> <p1>Bijzonderheden</p1> </label>
+									<textarea id="subject" name="subject" placeholder="Bijzonderheden" style="height:130px"></textarea>
+
+									<input type="submit" value="Verzend">                      </input>
+								</form>
+							</div>
+						</div>
+	<br>
+				</div>
+			</div>
+
+<!------------------------------------contact------------------------------------>
+			<div class="sectioncontact2">
+				<div class="container">
+					<h1>Contact</h1>
+						<div class="randje">
+							<div class="contactveld">
+
+								<form  action="action_page_contact.php" method="post">
+<br>
+									<label for="firstname"> </label>
+									<input type="text" id="firstname" name="firstname" placeholder="Uw naam">
+
+									<label for="email">  </label>
+									<input type="text" id="email" name="email" placeholder="Uw E-mail">
+
+
+
+
+<br>
+									<label for="subject"> <p1>Uw bericht</p1> </label>
+									<textarea id="subject" name="subject" placeholder="Uw bericht" style="height:130px"></textarea>
+
+									<input type="submit" value="Verzend">                      </input>
+								</form>
+								<br>
+								<p1>Lekker anders <br>
+									Baander 3 <br>
+									7811 HH Emmen <br>
+								</p1>
+
+							</div>
+						</div>
+	<br>
+				</div>
+
+
+
+
+
+
+			</div>
+
+
+
+
+
+	<div id="pagina12" class="pagina12">pagina12</div>
+
+
+
+	</div>
+
+
+
+
+<script src="jquery.js"></script>
+<script src="jquery.easing.1.3.min.js"></script>
+<script src="jquery.smoothPageScroll.js" type="text/javascript"></script>
+<script src="jquery.scroll.js" type="text/javascript"></script>
+
+<script type="text/javascript" src="carousel.js"></script>
+<script type="text/javascript" src="navbar highlight.js"></script>
+</body>
+</html>
