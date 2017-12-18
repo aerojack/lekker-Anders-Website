788742f9bddef4acf0bcd38283f2fb0a4fdafb5c
jorisgengler <jorisgengler@hotmail.com>
2017-12-18 16:43:07 +0100

    sublogo change to month

--- sublogo.js
+++ sublogo.js
@@ -4,17 +4,46 @@ $(document).ready(function(){
       document.getElementById("object").style.transform = "translate(750%,0) rotate(381deg)";
 
 
-      var current= new Date()
-      var day_night=current.getHours()
-      var Month = current.getMonth();
+      var current= new Date();
 
-      if (Month=12)
-        document.getElementById("object").src = "images/sublogo cg.png";
+      var maand = current.getMonth();
 
-      if (Month=11)
+
+      if (maand==0){ //January
         document.getElementById("object").src = "images/sublogo black.png";
-
-
-
+      }
+      if (maand==1){ //February
+        document.getElementById("object").src = "images/sublogo black.png";
+      }
+      if (maand==2){ //March
+        document.getElementById("object").src = "images/sublogo black.png";
+      }
+      if (maand==3){ //April
+        document.getElementById("object").src = "images/sublogo black.png";
+      }
+      if (maand==4){ //May
+        document.getElementById("object").src = "images/sublogo black.png";
+      }
+      if (maand==5){ //June
+        document.getElementById("object").src = "images/sublogo black.png";
+      }
+      if (maand==6){ //July
+        document.getElementById("object").src = "images/sublogo black.png";
+      }
+      if (maand==7){ //August
+        document.getElementById("object").src = "images/sublogo black.png";
+      }
+      if (maand==8){ //September
+        document.getElementById("object").src = "images/sublogo black.png";
+      }
+      if (maand==9){ //October
+        document.getElementById("object").src = "images/sublogo black.png";
+      }
+      if (maand==10){ //November
+        document.getElementById("object").src = "images/sublogo black.png";
+      }
+      if (maand==11) { // december
+        document.getElementById("object").src = "images/sublogo cg.png";
+      }
 
 });
