007e9e0456671a0f1a6a4c56140f1ab1480c9fd0
jorisgengler <jorisgengler@hotmail.com>
2017-12-18 16:13:08 +0100

    sub logo change by month

--- sublogo.js
+++ sublogo.js
@@ -4,5 +4,17 @@ $(document).ready(function(){
       document.getElementById("object").style.transform = "translate(750%,0) rotate(381deg)";
 
 
+      var current= new Date()
+      var day_night=current.getHours()
+      var Month = current.getMonth();
+
+      if (Month=12)
+        document.getElementById("object").src = "images/sublogo cg.png";
+
+      if (Month=11)
+        document.getElementById("object").src = "images/sublogo black.png";
+
+
+
 
 });
